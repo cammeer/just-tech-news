5a512dd2c3b4b77e2fedfeac736d1db7638a4da6
cardinal <caren@merzhaus.org>
2022-07-14 18:56:09 -0400

    sync with module code

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,30 +1,25 @@
-test("format_date() returns a date string", () => {
-    const date = new Date("2020-03-20 16:12:03");
+const {format_date, format_plural, format_url} = require('../utils/helpers');
 
-    expect(format_date(date)).toBe("3/20/2020");
+test('format_url() returns a simplified url string', () => {
+  const url1 = format_url('http://test.com/page/1');
+  const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+  const url3 = format_url('https://www.google.com?q=hello');
+
+  expect(url1).toBe('test.com');
+  expect(url2).toBe('coolstuff.com');
+  expect(url3).toBe('google.com');
 });
 
-const { format_date } = require("../utils/helpers");
+test('format_plural() returns a pluralized word', () => {
+  const word1 = format_plural('tiger', 1);
+  const word2 = format_plural('lion', 2);
 
-module.exports = {
-    format_date: (date) => {
-        // same logic as before...
-    },
-    format_plural: (word, amount) => {
-        if (amount !== 1) {
-            return `${word}s`;
-        }
+  expect(word1).toBe('tiger');
+  expect(word2).toBe('lions');
+});
 
-        return word;
-    },
-};
+test('format_date() returns a date string', () => {
+  const date = new Date('2020-03-20 16:12:03');
 
-test("format_url() returns a simplified url string", () => {
-    const url1 = format_url("http://test.com/page/1");
-    const url2 = format_url("https://www.coolstuff.com/abcdefg/");
-    const url3 = format_url("https://www.google.com?q=hello");
-
-    expect(url1).toBe("test.com");
-    expect(url2).toBe("coolstuff.com");
-    expect(url3).toBe("google.com");
+  expect(format_date(date)).toBe('3/20/2020');
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,18 +1,22 @@
 module.exports = {
-    format_date: (date) => {
-        return `${new Date(date).getMonth() + 1}/${new Date(
+  format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
       date
-    ).getDate()}/${new Date(date).getFullYear()}`;
-    },
-};
-
-//ask question about this thing below
-
-format_url: (url) => {
+    ).getFullYear()}`;
+  },
+  format_url: url => {
     return url
-        .replace("http://", "")
-        .replace("https://", "")
-        .replace("www.", "")
-        .split("/")[0]
-        .split("?")[0];
+      .replace('http://', '')
+      .replace('https://', '')
+      .replace('www.', '')
+      .split('/')[0]
+      .split('?')[0];
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
+  }
 };
